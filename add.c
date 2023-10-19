6eda32c363ce1ec401ec374775f25c2e42295921
ziliwalid <walidzili19@gmail.com>
2023-10-19 05:54:57 -0700

    commm

--- add.c
+++ add.c
@@ -1,20 +1,28 @@
 #include "monty.h"
+
 /**
- * addNodes - adds the top two elements of stack
- * @stck: stack param
- * @line_number: line num param
+ * addNodes - adds the top two elements of the stack
+ * @stck: stack parameter
+ * @line_number: line number parameter
  * Return: void
  */
 void addNodes(stack_t **stck, unsigned int line_number)
 {
 	int sum;
-
+	stack_t *current, *previous;
+	
 	if (stck == NULL || *stck == NULL || (*stck)->next == NULL)
+	{
 		errorHandler2(8, line_number, "add");
-
-	(*stck) = (*stck)->next;
-	sum = (*stck)->n + (*stck)->prev->n;
-	(*stck)->n = sum;
-	free((*stck)->prev);
-	(*stck)->prev = NULL;
+		return;
+	}
+	current = *stck;
+	previous = current->prev;
+	current = current->next;
+	/****************/
+	sum = current->n + previous->n;
+	current->n = sum;
+	free(previous);
+	current->prev = NULL;
 }
+
